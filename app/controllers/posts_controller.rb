f5bb10483eb5f9bc846dca5b64911cdf3886810a
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-26 17:38:44 -0400

    create action completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,11 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # add create method here
+  def create 
+    @post = Post.new #creates a new instance of Post that is evoked by the variable 'post'
+    @post.title = params[:title] #assigns 'post's' title with the input form data [:title]
+    @post.description = params[:description]  #assigns 'post's' description with the input form data [:description]
+    @post.save #.save generates a SQL script that inserts the new record with its newly defined attributes into the database
+    redirect_to post_path(@post) #this follows the convention of redirecting to the new resource's show page. As soon as this post is created, then, it redirects to that new post's show page. 
+  end
 end
